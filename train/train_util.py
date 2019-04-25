012fbfa6a4c6e6de1bd6af4a8c869fcf40d44fa3
tommy <haianyt@163.com>
2019-04-24 21:39:32 -0400

    fix bug

--- train/train_util.py
+++ train/train_util.py
@@ -1,31 +1,15 @@
-''' Util functions for training and evaluation.
+''' Utils for training.
 
 Author: Charles R. Qi
-Date: September 2017
+Date: October 2017
 '''
 
 import numpy as np
+from roi_seg_box3d_dataset import NUM_CLASS
 
-def get_batch(dataset, idxs, start_idx, end_idx,
-              num_point, num_channel,
-              from_rgb_detection=False):
-    ''' Prepare batch data for training/evaluation.
-    batch size is determined by start_idx-end_idx
-
-    Input:
-        dataset: an instance of FrustumDataset class
-        idxs: a list of data element indices
-        start_idx: int scalar, start position in idxs
-        end_idx: int scalar, end position in idxs
-        num_point: int scalar
-        num_channel: int scalar
-        from_rgb_detection: bool
-    Output:
-        batched data and label
-    '''
+def get_batch(dataset, idxs, start_idx, end_idx, num_point, num_channel, from_rgb_detection=False):
     if from_rgb_detection:
-        return get_batch_from_rgb_detection(dataset, idxs, start_idx, end_idx,
-            num_point, num_channel)
+        return get_batch_from_rgb_detection(dataset, idxs, start_idx, end_idx, num_point, num_channel)
 
     bsize = end_idx-start_idx
     batch_data = np.zeros((bsize, num_point, num_channel))
@@ -36,16 +20,13 @@ def get_batch(dataset, idxs, start_idx, end_idx,
     batch_size_class = np.zeros((bsize,), dtype=np.int32)
     batch_size_residual = np.zeros((bsize, 3))
     batch_rot_angle = np.zeros((bsize,))
-    if dataset.one_hot:
-        batch_one_hot_vec = np.zeros((bsize,3)) # for car,ped,cyc
+    if dataset.one_hot: batch_one_hot_vec = np.zeros((bsize,NUM_CLASS)) # for car,ped,cyc
     for i in range(bsize):
         if dataset.one_hot:
-            ps,seg,center,hclass,hres,sclass,sres,rotangle,onehotvec = \
-                dataset[idxs[i+start_idx]]
+            ps,seg,center,hclass,hres,sclass,sres,rotangle,onehotvec = dataset[idxs[i+start_idx]]
             batch_one_hot_vec[i] = onehotvec
         else:
-            ps,seg,center,hclass,hres,sclass,sres,rotangle = \
-                dataset[idxs[i+start_idx]]
+            ps,seg,center,hclass,hres,sclass,sres,rotangle = dataset[idxs[i+start_idx]]
         batch_data[i,...] = ps[:,0:num_channel]
         batch_label[i,:] = seg
         batch_center[i,:] = center
@@ -55,23 +36,16 @@ def get_batch(dataset, idxs, start_idx, end_idx,
         batch_size_residual[i] = sres
         batch_rot_angle[i] = rotangle
     if dataset.one_hot:
-        return batch_data, batch_label, batch_center, \
-            batch_heading_class, batch_heading_residual, \
-            batch_size_class, batch_size_residual, \
-            batch_rot_angle, batch_one_hot_vec
+        return batch_data, batch_label, batch_center, batch_heading_class, batch_heading_residual, batch_size_class, batch_size_residual, batch_rot_angle, batch_one_hot_vec
     else:
-        return batch_data, batch_label, batch_center, \
-            batch_heading_class, batch_heading_residual, \
-            batch_size_class, batch_size_residual, batch_rot_angle
+        return batch_data, batch_label, batch_center, batch_heading_class, batch_heading_residual, batch_size_class, batch_size_residual, batch_rot_angle
 
-def get_batch_from_rgb_detection(dataset, idxs, start_idx, end_idx,
-                                 num_point, num_channel):
+def get_batch_from_rgb_detection(dataset, idxs, start_idx, end_idx, num_point, num_channel):
     bsize = end_idx-start_idx
     batch_data = np.zeros((bsize, num_point, num_channel))
     batch_rot_angle = np.zeros((bsize,))
     batch_prob = np.zeros((bsize,))
-    if dataset.one_hot:
-        batch_one_hot_vec = np.zeros((bsize,3)) # for car,ped,cyc
+    if dataset.one_hot: batch_one_hot_vec = np.zeros((bsize,NUM_CLASS)) # for car,ped,cyc
     for i in range(bsize):
         if dataset.one_hot:
             ps,rotangle,prob,onehotvec = dataset[idxs[i+start_idx]]
